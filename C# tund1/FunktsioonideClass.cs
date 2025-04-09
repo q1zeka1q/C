ab22bad8fb4b8b04c485a548d6918483e5d66169
EvgeniyVasiliev <q1zeka1q@gmail.com>
2025-04-09 16:01:58 +0300

    C# tund 1

---
+++ C# tund1/FunktsioonideClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_alused
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello World! Привет! Tere päevast!");
+            // I. osa Andmetüübid, If, Case, Random, Alamfunktsioonid
+            int a = 0;
+            string tekst = "Python";
+            char taht = 'A';
+            double arv = 5.45435333353;
+            float arv1 = 2;
+            Console.Write("Mis on sinu nimi? ");
+            tekst = Console.ReadLine();
+            Console.WriteLine("Tere!\n" + tekst);
+            Console.WriteLine("\nTere {0}!", tekst);
+
+            // C# Valikute konstruktsionid
+            if (tekst.ToLower() == "juku")
+            {
+                Console.WriteLine("Lähme kinno!");
+                try
+                {
+                    Console.WriteLine("{0}\n Kui vana sa oled?", tekst);
+                    int vanus = int.Parse(Console.ReadLine());
+                    if (vanus <= 0 || vanus > 100)  // && - and, || - or
+                    {
+                        Console.WriteLine("Viga!");
+                    }
+                    else if (vanus > 0 && vanus <= 6)
+                    {
+                        Console.WriteLine("Tasuta");
+                    }
+                    else if (vanus < 15)
+                    {
+                        Console.WriteLine("Lastepilet");
+                    }
+                    else if (vanus >= 15 && vanus < 65)
+                    {
+                        Console.WriteLine("Täispilet!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sooduspilet!");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Olen hõivatud");
+            }
+
+            Console.WriteLine("Arv 2: ");
+            int arv2 = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+            arv1 = FunktsioonideClass.Kalkulaator(a, arv2);
+            Console.WriteLine(arv1);
+
+            Console.WriteLine("Switch'i kasutamine");
+            Random rnd = new Random();
+            a = rnd.Next(1, 7);
+            Console.WriteLine(a);
+
+            tekst = FunktsioonideClass.switchKasuta(a);
+            Console.WriteLine(tekst);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C# tund1/MainClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_alused
+{
+    class FunktsioonideClass
+    {
+        public static float Kalkulaator(int arv1, int arv2)
+        {
+            float kalkulaator = 0;
+            kalkulaator = arv1 * arv2;
+            return kalkulaator;
+        }
+
+        public static string switchKasuta(int a)
+        {
+            string tekst;
+
+            switch (a)
+            {
+                case 1: tekst = "Esmaspäev"; break;
+                case 2: tekst = "Teisipäev"; break;
+                case 3: tekst = "Kolmapäev"; break;
+                case 4: tekst = "Neljapäev"; break;
+                case 5: tekst = "Reede"; break;
+                case 6: tekst = "Laupäev"; break;
+                case 7: tekst = "Pühapäev"; break;
+
+                default:
+                    tekst = "Tundmatu";
+                    break;
+            }
+            return tekst;
+        }
+    }
+}
