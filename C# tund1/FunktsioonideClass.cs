4877ad866f32dbaed2125aa56c28029178ef392c
EvgeniyVasiliev <q1zeka1q@gmail.com>
2025-04-12 11:13:49 +0300

    lõpp

--- C# tund1/FunktsioonideClass.cs
+++ C# tund1/FunktsioonideClass.cs
@@ -9,33 +9,33 @@ namespace C_alused
 {
     class FunktsioonideClass
     {
-        //public static float Kalkulaator(int arv1, int arv2)
-        //{
-        //    float kalkulaator = 0;
-        //    kalkulaator = arv1 * arv2;
-        //    return kalkulaator;
-        //}
+        public static float Kalkulaator(int arv1, int arv2)
+        {
+            float kalkulaator = 0;
+            kalkulaator = arv1 * arv2;
+            return kalkulaator;
+        }
 
-        //public static string switchKasuta(int a)
-        //{
-        //    string tekst;
+        public static string switchKasuta(int a)
+        {
+            string tekst;
 
-        //    switch (a)
-        //    {
-        //        case 1: tekst = "Esmaspäev"; break;
-        //        case 2: tekst = "Teisipäev"; break;
-        //        case 3: tekst = "Kolmapäev"; break;
-        //        case 4: tekst = "Neljapäev"; break;
-        //        case 5: tekst = "Reede"; break;
-        //        case 6: tekst = "Laupäev"; break;
-        //        case 7: tekst = "Pühapäev"; break;
+            switch (a)
+            {
+                case 1: tekst = "Esmaspäev"; break;
+                case 2: tekst = "Teisipäev"; break;
+                case 3: tekst = "Kolmapäev"; break;
+                case 4: tekst = "Neljapäev"; break;
+                case 5: tekst = "Reede"; break;
+                case 6: tekst = "Laupäev"; break;
+                case 7: tekst = "Pühapäev"; break;
 
-        //        default:
-        //            tekst = "Tundmatu";
-        //            break;
-        //    }
-        //    return tekst;
-        //}
+                default:
+                    tekst = "Tundmatu";
+                    break;
+            }
+            return tekst;
+        }
         public static string pinginaabrid(string nimi1, string nimi2)
         {
             string vastus;
@@ -46,18 +46,124 @@ namespace C_alused
             else
             {
                 vastus = nimi1 + " ja " + nimi2 + " on pinginaabrid.";
-            }   
+            }
             return vastus;
         }
-        public static double korutamine(double a, double b)
+        public static double Korrutamine(double a, double b)
         {
             double vastus = a * b;
             return vastus;
         }
-        public static double korutamine(double a, double b)
-        {
 
+        public static double Hinnasoodustus(double a)
+        {
+            double vastus = a * 0.7;
+            return vastus;
+        }
+        public static string Temperatuur(int a)
+        {
+            string vastus;
+            if (a > 18)
+            {
+                vastus = "Temperatuur " + a + " on soovitav toasoojus talvel";
+            }
+            else
+            {
+                vastus = "Temperatuur ei ole soovitav talvel";
+            }
+            return vastus;
         }
 
+        public static string Pikkus(int a)
+        {
+            string vastus;
+            if (a < 160)
+            {
+                vastus = "Pikkus " + a + " cm, te olete lühikest kasvu";
+            }
+            else if (a >= 160 || a < 175)
+            {
+                vastus = "Pikkus " + a + " cm, te olete keskmist kasvu";
+            }
+            else
+            {
+                vastus = "Pikkus " + a + " cm, te olete pikka kasvu";
+            }
+            return vastus;
+        }
+
+        public static string PikkusSugu(int pikkus, string sugu)
+        {
+            string vastus;
+            if (sugu.ToLower() == "mees")
+            {
+                if (pikkus < 160)
+                {
+                    vastus = "Pikkus " + pikkus + " cm, te olete lühikest kasvu meeste jaoks";
+                }
+                else if (pikkus >= 160 || pikkus < 175)
+                {
+                    vastus = "Pikkus " + pikkus + " cm, te olete keskmist kasvu meeste jaoks";
+                }
+                else
+                {
+                    vastus = "Pikkus " + pikkus + " cm, te olete pikka kasvu meeste jaoks";
+                }
+            }
+            else if (sugu.ToLower() == "naine")
+            {
+                if (pikkus < 150)
+                {
+                    vastus = "Pikkus " + pikkus + " cm, te olete lühikest kasvu naiste jaoks";
+                }
+                else if (pikkus >= 160 || pikkus < 170)
+                {
+                    vastus = "Pikkus " + pikkus + " cm, te olete keskmist kasvu naiste jaoks";
+                }
+                else
+                {
+                    vastus = "Pikkus " + pikkus + " cm, te olete pikka kasvu naiste jaoks";
+                }
+            }
+            else
+            {
+                vastus = "Tundmatu sugu!";
+            }
+
+            return vastus;
+        }
+        public static string Pood(string vastus1, string vastus2, string vastus3)
+        {
+            float summa = 0;
+            string ostud = "Te ostsite: ";
+
+            if (vastus1.ToLower() == "jah")
+            {
+                summa += 1;
+                ostud += "piima ";
+            }
+            if (vastus2.ToLower() == "jah")
+            {
+                summa += 2;
+                ostud += "saia ";
+            }
+            if (vastus3.ToLower() == "jah")
+            {
+                summa += 3;
+                ostud += "leiba ";
+            }
+
+            if (summa == 0)
+            {
+                return "Te ei ostnud midagi.";
+            }
+            else
+            {
+                return ostud + "\nKokku maksumus: " + summa + " eurot.";
+            }
+
+         }
+    
     }
 }
+
--- C# tund1/MainClass.cs
+++ C# tund1/MainClass.cs
@@ -83,22 +83,59 @@ namespace C_alused
             string nimi1 = Console.ReadLine();
             Console.WriteLine("Küsin esimese inimese nimi?");
             string nimi2 = Console.ReadLine();
-            string tekst = FunktsioonideClass.pinginaabrid(nimi1, nimi2);
-            Console.WriteLine(tekst);
+            string tekst1 = FunktsioonideClass.pinginaabrid(nimi1, nimi2);
+            Console.WriteLine(tekst1);
 
 
-            Console.WriteLine("Kirjutage esimese seina suurus?");
-            int a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Kirjutage teise seina suurus?");
-            int b = int.Parse(Console.ReadLine());
-            int pindalaTulemus = (int)FunktsioonideClass.korutamine(a, b);
-            Console.WriteLine("Põranda pindala on: " + pindalaTulemus + " m²");
-            Console.WriteLine("Sisesta ruutmeetri hind:");
-            double hind = double.Parse(Console.ReadLine());
-            double maksumus = FunktsioonideClass.korutamine(pindalaTulemus, hind);
-            Console.WriteLine("Põranda vahetamise hind on: " + maksumus + " €");
+            Console.Write("Sisestage esimese seina pikkus:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sisestage teise seina pikkus:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double vastus = FunktsioonideClass.Korrutamine(a, b);
+            Console.WriteLine("Põranda pindala on " + vastus);
+            Console.Write("Kas soovite remodi teha?");
+            string vastus2 = Console.ReadLine();
+            if (vastus2.ToLower() == "jah")
+            {
+                Console.Write("Sisestage kui palju maksab ruutmeeter:");
+                double hind = Convert.ToDouble(Console.ReadLine());
+                double vastus3 = FunktsioonideClass.Korrutamine(hind, vastus);
+                Console.WriteLine("Põranda vahetamise hind on " + vastus3);
+            }
+            else
+            {
+                Console.WriteLine("Aitäh! Head aega!");
+            }
+            Console.Write("Sisestage alghind: ");
+            double alghind = Convert.ToDouble(Console.ReadLine());
+            vastus = FunktsioonideClass.Hinnasoodustus(alghind);
+            Console.WriteLine("Soodustusega 30% lõpphind on " + vastus);
 
+            Console.Write("Sisestage toa temperatuur: ");
+            int temp = int.Parse(Console.ReadLine());
+            vastus2 = FunktsioonideClass.Temperatuur(temp);
+            Console.WriteLine(vastus2);
 
+            Console.Write("Sisestage oma pikkus: ");
+            int pikkus = int.Parse(Console.ReadLine());
+            vastus2 = FunktsioonideClass.Pikkus(pikkus);
+            Console.WriteLine(vastus2);
+
+            Console.Write("Sisestage oma pikkus: ");
+            pikkus = int.Parse(Console.ReadLine());
+            Console.Write("Sisestage oma sugu: ");
+            string sugu = Console.ReadLine();
+            vastus2 = FunktsioonideClass.PikkusSugu(pikkus, sugu);
+            Console.WriteLine(vastus2);
+
+            Console.Write("Kas te soovite osta piima? (jah/ei): ");
+            string piim_vastus = Console.ReadLine();
+            Console.Write("Kas te soovite osta saia? (jah/ei): ");
+            string sai_vastus = Console.ReadLine();
+            Console.Write("Kas te soovite osta leiba? (jah/ei): ");
+            string leib_vastus = Console.ReadLine();
+            string tulemus = FunktsioonideClass.Pood(piim_vastus, sai_vastus, leib_vastus);
+            Console.WriteLine(tulemus);
 
             Console.ReadKey();
 
